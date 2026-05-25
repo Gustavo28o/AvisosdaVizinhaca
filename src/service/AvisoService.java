75660f9a8bcaadc7fd781ac6079e08313ad9b97b
Vinicius Lúcio Pourrat <vinicius.lucio0205@gmail.com>
2026-05-25 17:49:02 -0300

    Adicionar métodos sobrepostos.

--- src/service/AvisoService.java
+++ src/service/AvisoService.java
@@ -29,6 +29,23 @@ public class AvisoService {
     public int gerarProximoId() {
         return contador++;
     }
+ // Sobrecarga de métodos para criar avisos de diferentes formas
+    
+    public void criarAviso(String mensagem) {
+    System.out.println("Aviso geral: " + mensagem);
 }
-    // resto do código...
 
+public void criarAviso(String mensagem, String rua) {
+    System.out.println("Aviso para a rua " + rua + ": " + mensagem);
+}
+
+public void criarAviso(String mensagem, String rua, boolean urgente) {
+
+    if (urgente) {
+        System.out.println("AVISO URGENTE para " + rua + ": " + mensagem);
+    } else {
+        System.out.println("Aviso para " + rua + ": " + mensagem);
+    }
+}
+}
+   
